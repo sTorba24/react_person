edf2d97058350ef8dafcdf4136a6c5914d447c4d
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-10-11 09:12:21 +0300

    Updates

---
+++ precache-manifest.45a8cfaf82c3933c5731042559a5d653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f39ae37fdddff83862ed0bb31c8f00b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e8377b4c2cbe2eced2f",
+    "url": "./static/css/main.27cf0d04.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e8377b4c2cbe2eced2f",
+    "url": "./static/js/main.e6eb86fa.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
